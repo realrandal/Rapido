29581e624b5ad789ec35550cb33e5e27d0299cad
Artur <artur@agentejo.com>
2014-02-09 20:03:41 +0100

    route cleanup

--- config.php
+++ config.php
@@ -3,13 +3,14 @@
 // get realpath to docsroot
 $docsroot = str_replace(DIRECTORY_SEPARATOR, '/', isset($_SERVER['DOCUMENT_ROOT']) ? (is_link($_SERVER['DOCUMENT_ROOT']) ? readlink($_SERVER['DOCUMENT_ROOT']) : $_SERVER['DOCUMENT_ROOT']) : __DIR__);
 $base     = "/".trim(str_replace($docsroot, '', str_replace(DIRECTORY_SEPARATOR, '/', __DIR__)), "/");
+$base     = rtrim($base, '/');
 $svrname  = isset($_SERVER["SERVER_NAME"]) ? $_SERVER["SERVER_NAME"] : 'localhost';
 
 return [
     // don't touch, please!
     "name"         => $svrname,
-    "base_url"     => rtrim($base, '/'),
-    "base_route"   => rtrim($base, '/'),
+    "base_url"     => $base,
+    "base_route"   => $base,
     "docs_root"    => $docsroot,
     
     // edit here:
