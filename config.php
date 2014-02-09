a1657d04af98c7bcfef3ac7d3f7fc62bfbfbd673
Artur <artur@agentejo.com>
2014-02-09 19:47:24 +0100

    route fix

--- config.php
+++ config.php
@@ -2,14 +2,14 @@
 
 // get realpath to docsroot
 $docsroot = str_replace(DIRECTORY_SEPARATOR, '/', isset($_SERVER['DOCUMENT_ROOT']) ? (is_link($_SERVER['DOCUMENT_ROOT']) ? readlink($_SERVER['DOCUMENT_ROOT']) : $_SERVER['DOCUMENT_ROOT']) : __DIR__);
-$base     = "/".ltrim(str_replace($docsroot, '', str_replace(DIRECTORY_SEPARATOR, '/', __DIR__)), "/");
+$base     = "/".trim(str_replace($docsroot, '', str_replace(DIRECTORY_SEPARATOR, '/', __DIR__)), "/");
 $svrname  = isset($_SERVER["SERVER_NAME"]) ? $_SERVER["SERVER_NAME"] : 'localhost';
 
 return [
     // don't touch, please!
     "name"         => $svrname,
-    "base_url"     => $base,
-    "base_route"   => $base,
+    "base_url"     => rtrim($base, '/'),
+    "base_route"   => rtrim($base, '/'),
     "docs_root"    => $docsroot,
     
     // edit here:
