0aa50185664347316d4388fba5a1172a1fd8d885
Artur <artur@agentejo.com>
2014-02-09 22:28:43 +0100

    theme cleanup

--- content/index.php
+++ content/index.php
@@ -1,4 +1,9 @@
-<h1>Home</h1>
 
+<div class="uk-text-center">
 
-Congrats! You've installed Rapida successfully!
+    <h1>Congrats!</h1>
+    
+    <p class="uk-text-large">
+        You've installed Rapida successfully!
+    </p>
+</div>
--- themes/default/theme.php
+++ themes/default/theme.php
@@ -4,16 +4,21 @@
     <meta charset="UTF-8">
     <title>{{ $meta->title }}</title>
 
-    @scripts(['theme:assets/vendor/jquery.js','theme:assets/vendor/uikit/css/uikit.almost-flat.css', 'theme:assets/vendor/uikit/js/uikit.min.js'])
+    @scripts(['theme:assets/vendor/jquery.js','theme:assets/vendor/uikit/css/uikit.min.css', 'theme:assets/vendor/uikit/js/uikit.min.js'])
+    @scripts(['theme:assets/css/theme.css', 'theme:assets/js/theme.js'])
     @trigger("site.header")
 </head>
     <body>
         
         <div class="uk-container uk-container-center uk-margin-top">
             
-            @snippet("navigation")
+            <div class="uk-margin">
+                @snippet("navigation")
+            </div>
 
-            {{ $content_for_layout }}
+            <div class="uk-margin">
+                {{ $content_for_layout }}
+            </div>
         </div>
 
         @trigger("site.footer")
